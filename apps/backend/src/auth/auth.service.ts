598fdd64e51df5f682f471fc6c77b7a1240d1ca1
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-10-04 10:55:16 +0500

    backend: implement authentication service with JWT, local, and Google user validation

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,26 +1,103 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthInput } from './dto/create-auth.input';
-import { UpdateAuthInput } from './dto/update-auth.input';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { SignInInput } from './dto/signin.input';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { verify } from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { AuthJwtPayload } from './types/auth-jwtPayload';
+import { CreateUserInput } from 'src/user/dto/create-user.input';
+
+interface LoginInput {
+  id: number;
+  name: string;
+  email: string;
+  avatar: string | null;
+}
 
 @Injectable()
 export class AuthService {
-  create(createAuthInput: CreateAuthInput) {
-    return 'This action adds a new auth';
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateLocalUser({ email, password }: SignInInput) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) throw new UnauthorizedException('User Not Found');
+
+    if (!user.password) {
+      throw new UnauthorizedException('Invalid Credentials!');
+    }
+
+    const passwordMatched = await verify(user.password, password);
+
+    if (!passwordMatched)
+      throw new UnauthorizedException('Invalid Credentials!');
+
+    return user;
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async generateToken(userId: number): Promise<{ accessToken: string }> {
+    const payload: AuthJwtPayload = { sub: userId };
+    const accessToken: string = await this.jwtService.signAsync(payload);
+    return { accessToken };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  async login(user: LoginInput) {
+    const { accessToken } = await this.generateToken(user.id);
+    return {
+      id: user.id,
+      name: user.name,
+      avatar: user.avatar,
+      accessToken,
+    };
   }
 
-  update(id: number, updateAuthInput: UpdateAuthInput) {
-    return `This action updates a #${id} auth`;
+  async validateJwtUser(userId: number) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) throw new UnauthorizedException('User not found!');
+    const currentUser = { id: user.id };
+    return currentUser;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async validateGoogleUser(googleUser: CreateUserInput) {
+    let user = await this.prisma.user.findUnique({
+      where: { email: googleUser.email },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        avatar: true,
+        bio: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    if (!user) {
+      user = await this.prisma.user.create({
+        data: { ...googleUser },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          avatar: true,
+          bio: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+    }
+
+    return user;
   }
 }
