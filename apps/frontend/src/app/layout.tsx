8f3b55d73c3f8ef64adbf62e28751756de316086
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-10-13 21:39:03 +0500

    frontend: implement responsive navigation with desktop and mobile variants

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavbarContainer from "@/components/NavbarContainer";
+import Navbar from "@/components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <NavbarContainer>
+            <Navbar />
+          </NavbarContainer>
         {children}
       </body>
     </html>
