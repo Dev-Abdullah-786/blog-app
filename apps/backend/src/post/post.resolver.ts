f0c92fbe5bc5f81b1f12d93a57782d742379f2cd
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-10-19 11:51:14 +0500

    backend: add pagination and post count query to PostResolver and PostService

--- apps/backend/src/post/post.resolver.ts
+++ apps/backend/src/post/post.resolver.ts
@@ -1,13 +1,29 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args, Int, Context } from '@nestjs/graphql';
 import { PostService } from './post.service';
 import { Post } from './entities/post.entity';
+import { Request } from 'express';
+// import { UseGuards } from '@nestjs/common';
+// import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
 
 @Resolver(() => Post)
 export class PostResolver {
   constructor(private readonly postService: PostService) {}
 
+  // @UseGuards(JwtAuthGuard)
   @Query(() => [Post], { name: 'posts' })
-  findAll() {
-    return this.postService.findAll();
+  findAll(
+    @Context() context: { req: Request & { user?: any } },
+    @Args('skip', { nullable: true }) skip?: number,
+    @Args('take', { nullable: true }) take?: number,
+  ) {
+    const user = context.req.user;
+    console.log({ user });
+
+    return this.postService.findAll({ skip, take });
+  }
+
+  @Query(() => Int, { name: 'postCount' })
+  count() {
+    return this.postService.count();
   }
 }
