bdd5116c1cc068392f71d0f988fec29eee1c6485
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-11-04 08:29:02 +0500

    frontend: add SignUpForm component and use it in SignUpPage

---
+++ apps/frontend/src/app/auth/signup/_components/signUpForm.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import SubmitButton from "@/components/SubmitButton";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const SignUpForm = () => {
+  return (
+    <form className="flex flex-col gap-2">
+      <div>
+        <Label htmlFor="name">Name</Label>
+        <Input id="name" name="name" placeholder="John Doe" />
+      </div>
+
+      <div>
+        <Label htmlFor="email">Email</Label>
+        <Input id="email" name="email" placeholder="john@example.com" />
+      </div>
+
+      <div>
+        <Label htmlFor="password">Password</Label>
+        <Input id="password" name="password" type="password" />
+      </div>
+      <SubmitButton>Sign Up</SubmitButton>
+    </form>
+  );
+};
+
+export default SignUpForm;
