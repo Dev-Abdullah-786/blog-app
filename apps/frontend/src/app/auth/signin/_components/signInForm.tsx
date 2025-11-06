d9e61bde7183e61eaf4bb91555855b5efaafa975
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-11-06 09:33:19 +0500

    frontend: add sign-in form server action with validation and error handling

--- apps/frontend/src/app/auth/signin/_components/signInForm.tsx
+++ apps/frontend/src/app/auth/signin/_components/signInForm.tsx
@@ -2,12 +2,20 @@
 import SubmitButton from "@/components/SubmitButton";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { signIn } from "@/lib/actions/auth";
+import { useActionState } from "react";
 
 const SignInForm = () => {
+  const [state, action] = useActionState(signIn, undefined);
   return (
-    <form className="flex flex-col gap-2">
+    <form action={action} className="flex flex-col gap-2">
+      {!!state?.message && (
+        <p className="text-red-500 text-sm">{state.message}</p>
+      )}
       <div>
-        <Label htmlFor="email">Email</Label>
+        <Label defaultValue={state?.data.email} htmlFor="email">
+          Email
+        </Label>
         <Input
           id="email"
           name="email"
@@ -15,11 +23,22 @@ const SignInForm = () => {
           type="email"
         />
       </div>
+      {!!state?.errors?.email && (
+        <p className="text-red-500 text-sm">{state.errors.email}</p>
+      )}
 
       <div>
         <Label htmlFor="password">Password</Label>
-        <Input id="password" name="password" type="password" />
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          defaultValue={state?.data.password}
+        />
       </div>
+      {!!state?.errors?.password && (
+        <p className="text-red-500 text-sm">{state.errors.password}</p>
+      )}
 
       <SubmitButton>Sign In</SubmitButton>
     </form>
--- apps/frontend/src/lib/actions/auth.ts
+++ apps/frontend/src/lib/actions/auth.ts
@@ -3,8 +3,10 @@ import { SignUpFormState } from "../types/formState";
 import { SignUpFormSchema } from "../zodSchemas/signUpFormSchema";
 import { fetchGraphQL } from "../fetchGraphQL";
 import { print } from "graphql";
-import { CREATE_USER_MUTATION } from "../gqlQueries";
+import { CREATE_USER_MUTATION, SIGN_IN_MUTATION } from "../gqlQueries";
 import { redirect } from "next/navigation";
+import { LoginFormSchema } from "../zodSchemas/loginFormSchema";
+import { revalidatePath } from "next/cache";
 
 export async function signUp(
   state: SignUpFormState,
@@ -32,4 +34,34 @@ export async function signUp(
       message: "Something went wrong",
     };
   redirect("/auth/signin");
+}
+
+export async function signIn(
+  state: SignUpFormState,
+  formData: FormData
+): Promise<SignUpFormState> {
+  const validatedFields = LoginFormSchema.safeParse(
+    Object.fromEntries(formData.entries())
+  );
+
+  if (!validatedFields.success)
+    return {
+      data: Object.fromEntries(formData.entries()),
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+
+  const data = await fetchGraphQL(print(SIGN_IN_MUTATION), {
+    input: {
+      ...validatedFields.data,
+    },
+  });
+
+  if (data.errors) {
+    return {
+      data: Object.fromEntries(formData.entries()),
+      message: "Invalid Credentials",
+    };
+  }
+  revalidatePath("/");
+  redirect("/");
 }
