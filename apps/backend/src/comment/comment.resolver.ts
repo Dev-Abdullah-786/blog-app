e0ab6e190284ad5555e848b4c119fb4b025ab829
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-11-12 08:30:22 +0500

    backend: add Prisma integration and getPostComments query with pagination

--- apps/backend/src/comment/comment.resolver.ts
+++ apps/backend/src/comment/comment.resolver.ts
@@ -1,8 +1,28 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Args, Int } from '@nestjs/graphql';
 import { CommentService } from './comment.service';
 import { CommentEntity } from './entities/comment.entity';
+import { DEFAULT_PAGE_SIZE } from 'src/constants';
 
 @Resolver(() => CommentEntity)
 export class CommentResolver {
   constructor(private readonly commentService: CommentService) {}
+
+  @Query(() => [CommentEntity])
+  getPostComments(
+    @Args('postId', { type: () => Int }) postId: number,
+    @Args('take', {
+      type: () => Int,
+      nullable: true,
+      defaultValue: DEFAULT_PAGE_SIZE,
+    })
+    take: number,
+    @Args('skip', {
+      type: () => Int,
+      nullable: true,
+      defaultValue: 0,
+    })
+    skip: number,
+  ) {
+    return this.commentService.findOneByPost({ postId, take, skip });
+  }
 }
